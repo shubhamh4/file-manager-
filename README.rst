389100d945f76ba7a3be285985fe8b359963e44a
shubhamh4 <hedau.s10@gmail.com>
2018-02-04 13:07:09 +0530

    first commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+This is my App modules of a file manager I'm still working on it so 
+this is for testing Purpose only...
+The app name will be All-In-All: A all-in-one file solution for android
+
+Thank you.. 
---
+++ notificationDrawer/app/src/main/java/com/project/allinall/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.project.allinall;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+private DrawerLayout mDrawer;
+private ActionBarDrawerToggle mToggle;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+        mDrawer=(DrawerLayout) findViewById(R.id.drawer);
+        mToggle=new ActionBarDrawerToggle(this,mDrawer,R.string.open,R.string.close);
+        mDrawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
